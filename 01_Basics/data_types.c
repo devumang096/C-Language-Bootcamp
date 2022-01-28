91c1434763a71b936e23cf5c9a826b601326cdbb
devumang096 <devumang096@gmail.com>
2022-01-28 20:44:44 +0530

    datatypes

---
+++ 01_Basics/data_types.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    long int a=999999999;
+    unsigned int b= -63;
+    printf("%d\n",a);
+    printf("%d\n",b);
+    float c=3.5;
+    printf("%f",c);
+    return 0;
+}
