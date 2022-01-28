54976cc0fa9a61a83551178f8de7cb4c7f8c857a
devumang096 <devumang096@gmail.com>
2022-01-28 20:55:24 +0530

    multiplication

---
+++ 01_Basics/multiply.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,multiply;
+    printf("Enter two number:");
+    scanf("%d%d",&a,&b);
+    multiply=a*b;
+    printf("area of rectangle %d",multiply);
+    return 0;
+}
