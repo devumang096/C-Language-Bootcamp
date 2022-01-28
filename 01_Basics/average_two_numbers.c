a151d20abab886667a87333eabd17274e392eec2
devumang096 <devumang096@gmail.com>
2022-01-28 20:52:41 +0530

    average

---
+++ 01_Basics/average_two_numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int x,y;
+    float average;
+    printf("enter fist number",x);
+    scanf("%d",&x);
+    printf("enter second number",y);
+    scanf("%d",&y);
+    average= (x+y)/2.0;
+    printf("the average of two numbers =%f",average);
+    return 0;
+}
