86ea92154ded2d33e56c1d65899142fedd9f10e7
devumang096 <devumang096@gmail.com>
2022-02-01 22:07:55 +0530

    bitwise operators

---
+++ 01_Basics/Bitwise_operator.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter two numbers:",a,b);
+    scanf("%d %d",&a,&b);
+    int bitwise_and= a&b;
+    printf("%d\n",bitwise_and);
+    int bitwise_or = a|b;
+    printf("%d\n",bitwise_or);
+    int bitwise_xor = a^b;
+    printf("%d\n",bitwise_xor);
+    return 0;
+}
