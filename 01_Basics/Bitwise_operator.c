a17c1116ec432dffb5a261f00213124b185fbbd3
devumang096 <devumang096@gmail.com>
2022-02-02 21:11:52 +0530

    modified bitwise_file

--- 01_Basics/Bitwise_operator.c
+++ 01_Basics/Bitwise_operator.c
@@ -3,12 +3,14 @@ int main()
 {
     int a,b;
     printf("Enter two numbers:",a,b);
-    scanf("%d %d",&a,&b);
+    scanf("%d%d",&a,&b);
     int bitwise_and= a&b;
     printf("%d\n",bitwise_and);
     int bitwise_or = a|b;
     printf("%d\n",bitwise_or);
     int bitwise_xor = a^b;
     printf("%d\n",bitwise_xor);
+    int bitwise_compliment = (~a);
+    printf("%d\n",bitwise_compliment);
     return 0;
 }
