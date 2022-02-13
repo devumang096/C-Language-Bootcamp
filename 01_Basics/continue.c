e650725a558958b78dfda9ff726c1243ddcd77e9
devumang096 <devumang096@gmail.com>
2022-02-13 20:52:20 +0530

    continue-jump-statement

---
+++ 01_Basics/continue.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int addition(int x,int y)//global function
+{
+    int c;
+    c=x+y;
+    return c;
+}
+int main()
+{
+   // int x,y;
+  int x,y,c;
+    c=addition(10,20);
+    printf("%d",c);
+    return 0;
+}
